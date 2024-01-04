6f5dbfdf7d8e71198bccb691e02aefca71697103
NickolayD <145503799+NickolayD@users.noreply.github.com>
2024-01-04 19:06:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ async def main_handler(message: types.Message):
 async def bot_webhook(update: dict):
     telegram_update = types.Update(**update)
     #Dispatcher.set_current(dp)
-    dp.set_current()
+    #dp.set_current()
     Bot.set_current(bot)
     await dp.process_update(telegram_update)
 
