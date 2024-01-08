e0ef6c8be4d22c14510b3cb35c35e9e33cbe43ed
NickolayD <145503799+NickolayD@users.noreply.github.com>
2024-01-08 10:19:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,33 +8,35 @@ config = dotenv_values(".env")
 
 TELEGRAM_BOT_TOKEN= config['BOT_TOKEN'] 
 
-RENDER_WEB_SERVICE_NAME = "https://test-tgbot-service.onrender.com"
+WEB_SERVICE_NAME = 'https://7ccc-88-206-25-29.ngrok-free.app'
 
-START_BOT_TEXT = "Привет! Я - бот для распознавания различных овощей на фотографии."\
+START_BOT_TEXT = "Привет! Я - бот для распознавания различных овощей на фотографии. "\
     + "Отправь мне фотографию, если хочешь проверить мою работу."
     
 ANSWER_ON_TEXT = "Извини, я не понимаю твое сообщение. Вышли фото, если хочешь проверить мою работу."
 
 VEG_DICT = {
-    0: 'Бобы(Bean)',
-    1: 'Горькая тыква(Bitter Gourd)',
-    2: 'Бутылочная тыква(Botter Gourd)',
-    3: 'Баклажан(Brinjal)',
-    4: 'Брокколи(Broccoli)',
-    5: 'Капуста(Cabbage)',
-    6: 'Стручковый перец(Capsicum)',
-    7: 'Морковь(Carrot)',
-    8: 'Цветная капуста(Cauliflower)',
-    9: 'Огурец(Cucumber)',
-    10: 'Папайя(Papaya)',
-    11: 'Картофель(Potato)',
-    12: 'Тыква(Pumpkin)',
-    13: 'Редька(Radish)',
-    14: 'Томат(Tomato)',
+    0: 'Бобы (Bean)',
+    1: 'Горькая тыква (Bitter Gourd)',
+    2: 'Бутылочная тыква (Botter Gourd)',
+    3: 'Баклажан (Brinjal)',
+    4: 'Брокколи (Broccoli)',
+    5: 'Капуста (Cabbage)',
+    6: 'Стручковый перец (Capsicum)',
+    7: 'Морковь (Carrot)',
+    8: 'Цветная капуста (Cauliflower)',
+    9: 'Огурец (Cucumber)',
+    10: 'Папайя (Papaya)',
+    11: 'Картофель (Potato)',
+    12: 'Тыква (Pumpkin)',
+    13: 'Редька (Radish)',
+    14: 'Томат (Tomato)',
 }
 
-url = 'https://drive.google.com/uc?id=1o9KJoxkmT062OK7dyTGJafT19LIrcDWd'
-output = 'model.pkl'
+# для загрузки pickle-файла с обученной ML моделью
+url = 'https://drive.google.com/uc?id=14e4XLC96dYte0cF2XT8l9YMBSGb0JrXd'
+output = 'LinearSVCBest.pkl'
 gdown.download(url, output, quiet=False)
-with open("model.pkl", "rb") as file:
+with open(output, "rb") as file:
     MODEL = pickle.load(file)
+
